8f9749e39718345cab0d0ac3bb3f67f784d9fbaf
qgdct <877322443@qq.com>
2019-11-09 21:50:55 +0800

    测试1

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.gitdemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextView = this.findViewById(R.id.text_view);
+        mTextView.setText("9999999999999999999999999999999999");
     }
 }
