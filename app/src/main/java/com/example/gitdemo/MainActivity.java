845338b90303d5f46c30571154ae3db473f3d237
qgdct <877322443@qq.com>
2019-11-10 11:07:30 +0800

    测试3

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -8,14 +8,18 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextView;
+    private TextView mTextView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTextView1 = this.findViewById(R.id.text_view1);
         mTextView = this.findViewById(R.id.text_view);
+        mTextView1.setText("测试测试测试");
         mTextView.setSingleLine(true);
-        mTextView.setText("9999999999测试测试测试测试测试999999999999999999999999");
+        mTextView.setText("999999999966666666666666666666666666666999999999999");
+        mTextView.setTextColor(2222222);
     }
 }
