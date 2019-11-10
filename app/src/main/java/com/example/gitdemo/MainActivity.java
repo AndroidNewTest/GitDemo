1bf6c7f99227cc7ddcdccab049d0fd16fbb35a8c
qgdct <877322443@qq.com>
2019-11-10 11:19:22 +0800

    测试4

--- app/src/main/java/com/example/gitdemo/MainActivity.java
+++ app/src/main/java/com/example/gitdemo/MainActivity.java
@@ -8,6 +8,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextView;
+    private TextView mTextView3;
     private TextView mTextView2;
     private TextView mTextView1;
 
@@ -17,13 +18,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mTextView1 = this.findViewById(R.id.text_view1);
+        mTextView3 = this.findViewById(R.id.text_view3);
         mTextView2 = this.findViewById(R.id.text_view2);
         mTextView = this.findViewById(R.id.text_view);
         mTextView1.setText("测试测试测试");
         mTextView2.setSingleLine(true);
-        mTextView2.setText("9999999999999999999999");
+        mTextView3.setText("77777777777777777777777");
+        mTextView2.setText("6666666666666666666666");
         mTextView.setSingleLine(true);
-        mTextView.setText("9999999999999999999999");
+        mTextView.setText("55555555555555555555555");
         mTextView.setTextColor(2222222);
     }
 }
